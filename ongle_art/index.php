9af4599f129570b46aae312d62406d3d76069ef5
hanenkhelifi <haneenkhelifi@gmail.com>
2023-12-05 11:30:11 +0100

    footer

--- ongle_art/index.php
+++ ongle_art/index.php
@@ -143,7 +143,7 @@
 
                     <div class="row pt-5">
                         <div class="col-md-5 align-items-stretch">
-                            <img class="img" src="45.jpg" style="width: 480px ">
+                            <img class="img" src="44.jpg" style="width: 480px ">
 
                         </div>
 <br>
@@ -174,7 +174,154 @@
 
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
+<!--------------------------------footer-------------------------------!-->
 
+
+<!---------------------------------------------------------------------------->
+
+        <!------------------------------------------------------------------------------------------------------------------->
+
+        <html>
+
+        <head>
+            <meta charset="utf-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Untitled</title>
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css">
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
+            <link rel="stylesheet" href="assets/css/style.css">
+            <style>
+                .footer-dark {
+                    padding: 50px 0;
+                    color: #f0f9ff;
+                    background-color: #282d32;
+                }
+                
+                .footer-dark h3 {
+                    margin-top: 0;
+                    margin-bottom: 12px;
+                    font-weight: bold;
+                    font-size: 16px;
+                }
+                
+                .footer-dark ul {
+                    padding: 0;
+                    list-style: none;
+                    line-height: 1.6;
+                    font-size: 14px;
+                    margin-bottom: 0;
+                }
+                
+                .footer-dark ul a {
+                    color: inherit;
+                    text-decoration: none;
+                    opacity: 0.6;
+                }
+                
+                .footer-dark ul a:hover {
+                    opacity: 0.8;
+                }
+                
+                @media (max-width:767px) {
+                    .footer-dark .item:not(.social) {
+                        text-align: center;
+                        padding-bottom: 20px;
+                    }
+                }
+                
+                .footer-dark .item.text {
+                    margin-bottom: 36px;
+                }
+                
+                @media (max-width:767px) {
+                    .footer-dark .item.text {
+                        margin-bottom: 0;
+                    }
+                }
+                
+                .footer-dark .item.text p {
+                    opacity: 0.6;
+                    margin-bottom: 0;
+                }
+                
+                .footer-dark .item.social {
+                    text-align: center;
+                }
+                
+                @media (max-width:991px) {
+                    .footer-dark .item.social {
+                        text-align: center;
+                        margin-top: 20px;
+                    }
+                }
+                
+                .footer-dark .item.social>a {
+                    font-size: 20px;
+                    width: 36px;
+                    height: 36px;
+                    line-height: 36px;
+                    display: inline-block;
+                    text-align: center;
+                    border-radius: 50%;
+                    box-shadow: 0 0 0 1px rgba(255, 255, 255, 0.4);
+                    margin: 0 8px;
+                    color: #fff;
+                    opacity: 0.75;
+                }
+                
+                .footer-dark .item.social>a:hover {
+                    opacity: 0.9;
+                }
+                
+                .footer-dark .copyright {
+                    text-align: center;
+                    padding-top: 24px;
+                    opacity: 0.3;
+                    font-size: 13px;
+                    margin-bottom: 0;
+                }
+                
+                https: //epicbootstrap.com/snippets/footer-dark
+            </style>
+        </head>
+
+        <body>
+            <div class="footer-dark">
+                <footer>
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-sm-6 col-md-3 item">
+                                <h3>Services</h3>
+                                <ul>
+                                    <li><a href="#">Vernis</a></li>
+                                    <li><a href="#">Soins</a></li>
+                                    <li><a href="#">Design</a></li>
+                                </ul>
+                            </div>
+                            <div class="col-sm-6 col-md-3 item">
+                                <h3>About</h3>
+                                <ul>
+                                    <li><a href="#">Company</a></li>
+                                    <li><a href="#">Team</a></li>
+                                    <li><a href="#">Careers</a></li>
+                                </ul>
+                            </div>
+                            <div class="col-md-6 item text">
+                                <h3>Ongle Art</h3>
+                                <p>De précieuses minutes de bien-être pour mieux affronter le quotidien.</p>
+                            </div>
+                            <div class="col item social"><a href="#"><i class="icon ion-social-facebook"></i></a><a href="#"><i class="icon ion-social-twitter"></i></a><a href="#"><i class="icon ion-social-snapchat"></i></a><a href="#"><i class="icon ion-social-instagram"></i></a></div>
+                        </div>
+                        <p class="copyright">OnglesArt © 2023</p>
+                    </div>
+                </footer>
+            </div>
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/js/bootstrap.bundle.min.js"></script>
+        </body>
+
+
+</html>
 </body>
 
 </html>
