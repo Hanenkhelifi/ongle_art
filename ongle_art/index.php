b5270c6e5540b4e7e11e3b09e93df0b504ecb2ab
hanenkhelifi <haneenkhelifi@gmail.com>
2023-12-05 21:39:20 +0100

    ..

--- ongle_art/index.php
+++ ongle_art/index.php
@@ -12,7 +12,7 @@
     <!--css -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="./css/style.css">
-    <title>site web ongle art </title>
+    <title>ongle art </title>
 </head>
 
 <body>
